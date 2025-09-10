aa2844f5b03d94e6f53f511a8b80a6dbef82d163
js4941662-max <abrahamtrueba@icloud.com>
2025-09-09 20:26:30 -0500

    Fix bundle identifier: com.floramind.aiplants (removed double 'ai')

--- app.config.js
+++ app.config.js
@@ -24,7 +24,7 @@ module.exports = ({ config }) => {
       },
       ios: {
         supportsTablet: false,
-        bundleIdentifier: "com.floramind.aiplantai",
+        bundleIdentifier: "com.floramind.aiplants",
         buildNumber: "11",
         requireFullScreen: true,
         deviceFamily: [1, 2],
@@ -54,7 +54,7 @@ module.exports = ({ config }) => {
         },
         associatedDomains: ["applinks:floramind.app"],
         entitlements: {
-          "com.apple.developer.in-app-payments": ["merchant.com.floramind.aiplantai"],
+          "com.apple.developer.in-app-payments": ["merchant.com.floramind.aiplants"],
           "com.apple.developer.associated-domains": true
         }
       },
@@ -63,7 +63,7 @@ module.exports = ({ config }) => {
           foregroundImage: "./assets/adaptive-icon.png",
           backgroundColor: "#1B5E20"
         },
-        package: "com.floramind.aiplantai",
+        package: "com.floramind.aiplants",
         permissions: [
           "android.permission.CAMERA",
           "android.permission.READ_EXTERNAL_STORAGE",
